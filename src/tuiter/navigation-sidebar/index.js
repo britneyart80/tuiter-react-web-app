41de64c9eebde3ea1cfb1b64105af9395431ed8d
britneyart80 <britneychen@Britneys-MacBook-Pro.local>
2022-10-18 12:03:33 -0400

    a6

---
+++ src/tuiter/navigation-sidebar/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./index.css";
+
+const NavigationSidebar = ({ active = "explore" }) => {
+  return (
+    <div className="list-group wd-nav-sidebar">
+      <a className="list-group-item">Tuiter</a>
+      <a
+        className={`list-group-item
+                    ${active === "home" ? "active" : ""}`}
+      >
+        Home
+      </a>
+      <a
+        className={`list-group-item
+                    ${active === "explore" ? "active" : ""}`}
+      >
+        Explore
+      </a>
+      <a
+        className={`list-group-item
+                    ${active === "notifications" ? "active" : ""}`}
+      >
+        Notifications
+      </a>
+      <a
+        className={`list-group-item
+                    ${active === "messages" ? "active" : ""}`}
+      >
+        Messages
+      </a>
+      <a
+        className={`list-group-item
+                    ${active === "bookmarks" ? "active" : ""}`}
+      >
+        Bookmarks
+      </a>
+      <a
+        className={`list-group-item
+                    ${active === "lists" ? "active" : ""}`}
+      >
+        Lists
+      </a>
+      <a
+        className={`list-group-item
+                    ${active === "profile" ? "active" : ""}`}
+      >
+        Profile
+      </a>
+      <a
+        className={`list-group-item
+                    ${active === "more" ? "active" : ""}`}
+      >
+        More
+      </a>
+    </div>
+  );
+};
+export default NavigationSidebar;
